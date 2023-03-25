dd49a3a4620e23e55149cd72006ae603a918d114
YassineDeriouch <yderouich14@gmail.com>
2023-03-24 18:08:55 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
- /**
- * print_triangle - Prints a triangle, using the character #.
- * @size: The size of the triangle.
- */
+/**
+* print_triangle - Prints a triangle, using the character #.
+* @size: The size of the triangle.
+*/
 void print_triangle(int size)
 {
 	int hash, index;
