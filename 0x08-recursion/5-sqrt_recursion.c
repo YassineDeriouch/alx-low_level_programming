673a53f9847dabde43c15112344b7eb051153d8c
YassineDerouich <yderouich14@gmail.com>
2023-04-05 17:15:55 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,47 +1,36 @@
 #include "main.h"
-int _root_sqrt(int raiz, int cont);
 
 /**
- * _sqrt_recursion - function that returns the natural square root of a number
- *@n: first value
- *
- * Return: Return solution
+ * _sqrt - finds the square root of a value
+ * @prev: previous value
+ * @root: the square value
+ * Return: the square root
+ */
+int _sqrt(int prev, int root)
+{
+	if (prev > root)
+	{
+		return (-1);
+	}
+	else if (prev * prev == root)
+	{
+		return (prev);
+	}
+
+	return (_sqrt(prev + 1, root));
+}
+
+/**
+ * _sqrt_recursion - recursive square root of a number
+ * @n: the integer
+ * Return: square root
  */
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-	{
-		return (0);
-	}
 	if (n < 0)
 	{
 		return (-1);
 	}
-	else
-	{
-		return (_root_sqrt(n, 1));
-	}
-}
 
-/**
- * _root_sqrt - calculate the square root
- *@raiz: value to calc the square root
- *@cont: variable to cont
- *
- * Return: the number with the solution
- */
-int _root_sqrt(int raiz, int cont)
-{
-	if (cont * cont == raiz)
-	{
-		return (cont);
-	}
-	else if (cont * cont > raiz)
-	{
-	return (-1);
-	}
-	else
-	{
-		return (_root_sqrt(raiz, cont + 1));
-	}
+	return (_sqrt(1, n));
 }
